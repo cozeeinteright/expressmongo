1ed0b001080eeff2012bd9307014aa89cc264fba
koji noguchi <info@interight.co.jp>
2022-07-17 06:27:32 +0000

    add formik

--- front/src/App.js
+++ front/src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Home from './page/Home'
 import About from './page/About'
+import ShowLoginform from './components/ShowLoginform';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello
+        <ShowLoginform />
       </header>
       <BrowserRouter>
-      <  Routes>
+      <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
         </Routes>
