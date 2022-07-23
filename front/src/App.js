81863a851547275cd889ca21cabbab75570fee38
koji noguchi <info@interight.co.jp>
2022-07-23 11:53:18 +0000

    add MUI css

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,29 @@
-import './App.css';
+import CssbaseLine from '@mui/material/CssBaseline'
+import Grid from '@mui/material/Grid';
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Home from './page/Home'
 import About from './page/About'
 import ShowLoginform from './components/ShowLoginform';
+import Item from './components/Item'
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <CssbaseLine />
+      <Grid container spacing={2}>
+  <Grid item xs={8}>
+    <Item>xs=8</Item>
+  </Grid>
+  <Grid item xs={4}>
+    <Item>xs=4</Item>
+  </Grid>
+  <Grid item xs={4}>
+    <Item>xs=4</Item>
+  </Grid>
+  <Grid item xs={8}>
+    <Item>xs=8</Item>
+  </Grid>
+</Grid>
       <ShowLoginform />
       <header className="App-header">
         test app
@@ -17,7 +34,7 @@ function App() {
           <Route path="about" element={<About />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </>
   );
 }
 
