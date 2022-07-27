7aade42789cd419cb55b1d45f8a99b2aa4604443
koji noguchi <info@interight.co.jp>
2022-07-27 12:56:52 +0000

    add Signin.tsx and yarn add yup

--- tsfront/src/App.tsx
+++ tsfront/src/App.tsx
@@ -1,79 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { blue } from '@mui/material/colors';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box'
+
 import Home from './page/Home'
 import About from './page/About'
-import AnchorText from './components/AnchorText'
-import MenuButton from './components/MenuButton'
-import MenuList from './components/MenuList'
-
-const theme = createTheme({
-  palette: {
-    primary: blue,
-  },
-});
-
-const groundTopbar = {
-  display: 'flex',
-  justifyContent: 'center',
-  bgcolor: `${theme.palette.primary.light}`,
-} as const
-
-const innerTopbar = {
-  display: 'flex',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  justifyContent: 'space-between',
-  width: '100%',
-  maxWidth: 1200,
-} as const
-
-const title = {
-  p: 2,
-  color: 'white',
-} as const
-
-const menubutton = {
-  p: 2,
-  display: { xs: 'block', sm: 'block', md: 'none', lg: 'none', xl: 'none' },
-} as const
-
-const menulist = {
-  p: 2,
-  display: { xs: 'none', sm: 'none', md: 'block', lg: 'block', xl: 'block' },
-} as const
+import SignIn from './page/SignIn'
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <header>
-          <Box sx={groundTopbar}>
-            <Box sx={innerTopbar}>
-              <Box sx={title}>Satoyama Biodiversiry Project</Box>
-              <Box sx={menubutton}><MenuButton /></Box>
-              <Box sx={menulist}><MenuList /></Box>
-            </Box>
-          </Box>
-          <AnchorText />
-        </header>
-        <main>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="about" element={<About />} />
-            </Routes>
-          </BrowserRouter>        
-        </main>
-        <footer>
-          <Button variant="contained">Contained</Button>
-        </footer>
-    </ThemeProvider>
+      satoyama
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="signin" element={<SignIn />} />
+        </Routes>
+      </BrowserRouter> 
     </>
   );
 }
--- tsfront/src/page/Home.tsx
+++ tsfront/src/page/Home.tsx
@@ -2,7 +2,26 @@ import { Link } from "react-router-dom"
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 
-const section = {
+import { blue } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Button from '@mui/material/Button';
+import AnchorText from './../components/AnchorText'
+import MenuButton from './../components/MenuButton'
+import MenuList from './../components/MenuList'
+
+const theme = createTheme({
+  palette: {
+    primary: blue,
+  },
+});
+const groundTopbar = {
+  display: 'flex',
+  justifyContent: 'center',
+  bgcolor: `${theme.palette.primary.light}`,
+} as const
+
+const innerTopbar = {
   display: 'flex',
   flexDirection: 'row',
   flexWrap: 'wrap',
@@ -11,18 +30,51 @@ const section = {
   maxWidth: 1200,
 } as const
 
+const title = {
+  p: 2,
+  color: 'white',
+} as const
+
+const menubutton = {
+  p: 2,
+  display: { xs: 'block', sm: 'block', md: 'none', lg: 'none', xl: 'none' },
+} as const
+
+const menulist = {
+  p: 2,
+  display: { xs: 'none', sm: 'none', md: 'block', lg: 'block', xl: 'block' },
+} as const
+
 function  Home() {
     return (
       <>
-        <Box sx={section}>
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <header>
+        <Box sx={groundTopbar}>
+          <Box sx={innerTopbar}>
+            <Box sx={title}>Satoyama Biodiversiry Project</Box>
+            <Box sx={menubutton}><MenuButton /></Box>
+            <Box sx={menulist}><MenuList /></Box>
+          </Box>
+        </Box>
+        <AnchorText />
+      </header>
+      <main>
+      <Box >
           <Typography component="h2">
-            h2. Heading
+            We belive
           </Typography>;
           <nav>
             <Link to="/about">About</Link>
           </nav>
-        </Box>
-      </>
+        </Box>    
+      </main>
+      <footer>
+        <Button variant="contained">Contained</Button>
+      </footer>
+    </ThemeProvider>
+    </>
     );
   }
 
